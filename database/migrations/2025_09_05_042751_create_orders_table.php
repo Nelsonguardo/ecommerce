67629005599f10eb7bbfddc37b8eaf4b60909279
Nelsonguardo <guardonei4@gmail.com>
2025-09-07 13:06:28 -0500

    Add models and migrations for Address, Brand, Category, Order, OrderItem, and Product with relationships and fillable attributes

--- database/migrations/2025_09_05_042751_create_orders_table.php
+++ database/migrations/2025_09_05_042751_create_orders_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Decimal;
+
+use function Laravel\Prompts\text;
 
 return new class extends Migration
 {
@@ -13,6 +16,16 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->decimal('grand_total', 10, 2)->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->enum('status', ['new','processing','shipped', 'delivered', 'cancelled'])->default('new');
+            $table->string('currency')->nullable();
+            $table->decimal('shipping_amount',10,2)->nullable();
+            $table->string('shipping_method')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
